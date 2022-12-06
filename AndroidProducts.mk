369fe6eeb2e1017ee62c2f1e35dcc66f39af236b
LeDrew2017 <ledrew101806@gmail.com>
2022-12-05 20:12:10 -0500

    added banana-13

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_d2s.mk
+    $(LOCAL_DIR)/banana_d2s.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_d2s-eng \
-    xtended_d2s-user \
-    xtended_d2s-userdebug
+    banana_d2s-eng \
+    banana_d2s-user \
+    banana_d2s-userdebug
